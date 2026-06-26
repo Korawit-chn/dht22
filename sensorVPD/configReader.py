8e2956962fa8b2fad0e8f6660493efe8f1f7f84d
Korawit-chn <workingforbeingnothing@gmail.com>
2026-06-26 09:26:33 +0000

    Change read .txt to .json

--- sensorVPD/configReader.py
+++ sensorVPD/configReader.py
@@ -1,5 +1,7 @@
+import json
 import uuid
 from pathlib import Path
+
 
 def get_device_uuid():
     uuid_file = Path("device_uuid.txt")
@@ -11,31 +13,18 @@ def get_device_uuid():
 
 
 def readConfig(filename):
-    sensor_type = None
-    location = None
-    gpio = None
-
     with open(filename, "r") as file:
-        for line in file:
-            parts = line.strip().split(": ", 1)
+        config_data = json.load(file)
 
-            if len(parts) != 2:
-                continue
-
-            key, value = parts
-
-            if key == "Type":
-                sensor_type = value
-
-            elif key == "Location":
-                location = value
-
-            elif key == "GPIO":
-                gpio = value
+    sensor_type = config_data.get("Type") or config_data.get("sensorType") or config_data.get("type")
+    location = config_data.get("Location") or config_data.get("locationName") or config_data.get("location")
+    gpio = config_data.get("GPIO") or config_data.get("gpio")
+    description = config_data.get("description") or config_data.get("Description") or config_data.get("desc")
 
     return {
         "deviceUUID": get_device_uuid(),
         "sensorType": sensor_type,
         "locationName": location,
-        "gpio": gpio
+        "gpio": gpio,
+        "description": description,
     }
